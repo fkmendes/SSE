f6a41746d77505c5ef0b33709020fa55e04275aa
JEFFREY ZHANG <jozhang@berkeley.edu>
2018-10-11 14:57:36 +1300

    writes graph for error plots

---
+++ construct_error_graph.py
@@ -0,0 +1,24 @@
+import pandas
+import matplotlib.pyplot as plt
+import argparse
+import numpy as np
+
+def plot(name, use_log):
+    df = pandas.read_csv(name, index_col=0, header=None)
+    n_samples, n_nodes = df.shape
+    for i in range(1, n_nodes + 1):
+        fig, ax = plt.subplots()
+        plt.title(name)
+        if False and use_log:
+            ax.scatter(np.log(df.index), df[i])
+        else:
+            ax.scatter(df.index, df[i])
+        fig.savefig("plots/" + name + str(i) + '.png', dpi=300)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="Generate asr validation", description="Generates ancestral state reconstruction figures that validate method.")
+    parser.add_argument("-n", "--csv-name", action="store", dest="csv", default=None, type=str, help="CSV file containing post data.")
+    parser.add_argument("-l", "--log-scale", action="store", dest="log", default=False, type=bool, help=".")
+    args = parser.parse_args()
+
+    plot(args.csv, args.log)
